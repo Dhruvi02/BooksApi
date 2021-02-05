44f97e0cdc92835443cd2975943fcc9fac7c4075
NSP User <dkoladia208@rku.ac.in>
2021-02-05 17:49:45 +0530

    Database connection changes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const bodyParser = require('body-parser')
 const route = require("./route")
 
 
-mongoose.connect("mongodb://localhost:27017/books_db",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb://mongodb+srv://user_dk:user_dk@cluster0.ca8ee.mongodb.net/Studentdb?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         var app = express();
         app.use(bodyParser.urlencoded({extended:true}));
         app.use('/api',route)
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log("Server started...!!")
         })
     }
