55a3a2705d84ef6deecbd5032e182e4bbb9bdec5
Dhruvi02 <63729267+Dhruvi02@users.noreply.github.com>
2021-02-05 19:43:10 +0530

    Update route.js

--- route.js
+++ route.js
@@ -12,44 +12,44 @@ router.get("/books",async (req,res)=>{
     })
 })
 
-router.post('/books', async (req,res)=>{
+// router.post('/books', async (req,res)=>{
 
-    const book = new Book({
-        b_title : req.body.b_title,
-        b_author : req.body.b_author,
-        b_qty : req.body.b_qty,
-        b_adition : req.body.b_adition,
-        b_cost : req.body.b_cost
-    })
+//     const book = new Book({
+//         b_title : req.body.b_title,
+//         b_author : req.body.b_author,
+//         b_qty : req.body.b_qty,
+//         b_adition : req.body.b_adition,
+//         b_cost : req.body.b_cost
+//     })
     
-    await book.save();
-    res.status(200).json({
-        msg: book
-    })
+//     await book.save();
+//     res.status(200).json({
+//         msg: book
+//     })
 
-})
+// })
 
-router.delete("/books/:id",async (req,res)=>{
-    var id = req.params.id
-     Book.remove({_id: id},(err,data)=>{
-            if(!err){
-                res.send(data)
-            }else{
-                res.send(err)
-            }
-        })
-    })
+// router.delete("/books/:id",async (req,res)=>{
+//     var id = req.params.id
+//      Book.remove({_id: id},(err,data)=>{
+//             if(!err){
+//                 res.send(data)
+//             }else{
+//                 res.send(err)
+//             }
+//         })
+//     })
 
-router.patch("/books/:id",async(req,res)=>{
-    var id = ObjectId(req.params.id);
-    const UpdateBook = req.body
-    const data = await Book.findOneAndUpdate({_id:id},UpdateBook)
-    if (!data) {
-        res.send("Match not found...!!")
-    } else {
-        res.send("Updated sucessfully...!!")
-    }
-})
+// router.patch("/books/:id",async(req,res)=>{
+//     var id = ObjectId(req.params.id);
+//     const UpdateBook = req.body
+//     const data = await Book.findOneAndUpdate({_id:id},UpdateBook)
+//     if (!data) {
+//         res.send("Match not found...!!")
+//     } else {
+//         res.send("Updated sucessfully...!!")
+//     }
+// })
 
 
 module.exports = router;
